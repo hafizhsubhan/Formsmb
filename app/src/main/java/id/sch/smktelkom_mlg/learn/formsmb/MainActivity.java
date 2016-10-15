ec14f30f98f40fe571346b78ee3be2bb5e0cac17
Hafizh Subhan Mahdiy <hafizhkerabat1@gmail.com>
2016-10-16 01:04:27 +0700

    +Skrip widget. +Semua Widget berhasil berfungsi +menghitung Usia. +Panggilan Bapak/Ibu +CheckBox Syarat dan Ketentuan

--- app/src/main/java/id/sch/smktelkom_mlg/learn/formsmb/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/formsmb/MainActivity.java
@@ -4,16 +4,20 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.RadioGroup;
+import android.widget.RadioButton;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText etNama, etTelp, etAlamat, etEmail;
+    EditText etNama, etTelp, etAlamat, etEmail, etTahun, etTanggal, etBulan;
     TextView tvHasil;
     Button btOK;
-    RadioGroup rgKelamin;
+    RadioButton rbL, rbP;
+    Spinner spRayon;
+    CheckBox syarat;
 
 
     @Override
@@ -27,7 +31,13 @@ public class MainActivity extends AppCompatActivity {
         etEmail = (EditText) findViewById(R.id.editTextEmail);
         btOK = (Button) findViewById(R.id.buttonOk);
         tvHasil = (TextView) findViewById(R.id.textViewHasil);
-        rgKelamin = (RadioGroup) findViewById(R.id.radioGroupKelamin);
+        rbL = (RadioButton) findViewById(R.id.radioButtonPria);
+        rbP = (RadioButton) findViewById(R.id.radioButtonWanita);
+        spRayon = (Spinner) findViewById(R.id.spinnerRayon);
+        syarat = (CheckBox) findViewById(R.id.checkBoxSyarat);
+        etTahun = (EditText) findViewById(R.id.editTextTahun);
+        etTanggal = (EditText) findViewById(R.id.editTextTanggal);
+        etBulan = (EditText) findViewById(R.id.editTextBulan);
 
         btOK.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,11 +56,29 @@ public class MainActivity extends AppCompatActivity {
             String telp = etTelp.getText().toString();
             String alamat = etAlamat.getText().toString();
             String email = etEmail.getText().toString();
+            String panggil = null;
+            String sk = null;
+            String rayon = spRayon.getSelectedItem().toString();
 
+            int usia = Integer.parseInt(etTahun.getText().toString());
+            usia = 2016 - usia;
 
-            tvHasil.setText("Anda bernama " + nama + " dengan nomor" +
+            if (rbL.isChecked()) {
+                panggil = "Bapak";
+            } else if (rbP.isChecked()) {
+                panggil = "Ibu";
+            }
+
+            if (syarat.isChecked()) {
+                sk = "Menyetujui";
+            } else {
+                sk = "tidak menyetujui";
+            }
+
+            tvHasil.setText("Anda " + panggil + " " + nama + " berusia " + usia + " tahun, dengan nomor" +
                     " yang dapat dihubungi " + telp + " dan Email " +
-                    email + " yang bertempat tinggal di " + alamat);
+                    email + " yang bertempat tinggal di " + alamat + ". Telah memilih" +
+                    "rayon " + rayon + " serta " + sk + " syarat dan ketentuan");
         }
 
     }
@@ -62,7 +90,14 @@ public class MainActivity extends AppCompatActivity {
         String telp = etTelp.getText().toString();
         String alamat = etAlamat.getText().toString();
         String email = etEmail.getText().toString();
+        String tahun = etTahun.getText().toString();
+        String tanggal = etTanggal.getText().toString();
+        String bulan = etBulan.getText().toString();
 
+        if (tahun.isEmpty() || bulan.isEmpty() || tanggal.isEmpty()) {
+            etTahun.setError("Kapan Anda Lahir?");
+            valid = false;
+        }
 
         if (nama.isEmpty()) {
             etNama.setError("Nama Belum Diisi!");
